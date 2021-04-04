61bd4b6902af4ce2a7887d6dfea642fac36282db
paschalidi <paschalidi@outlook.com>
2021-04-04 21:15:56 +0200

    chore: lints

--- src/common/components/UIElements/Logo/index.js
+++ src/common/components/UIElements/Logo/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
+import NextLink from "next/link";
 import Text from "../../Text";
 import Link from "../../Link";
 import Image from "../../Image";
-import NextLink from "next/link";
 
 const Logo = ({
   logoWrapperStyle,
--- src/containers/Navbar/index.js
+++ src/containers/Navbar/index.js
@@ -1,36 +1,34 @@
 import React from "react";
+import Link from "next/link";
 import Logo from "../../common/components/UIElements/Logo";
 import Image from "../../common/components/Image";
 import Container from "../../common/components/UI/Container";
 import NavbarWrapper, { MenuWrapper, Button } from "./navbar.style";
-import Link from "next/link";
 
 import logoImage from "../../common/assets/image/charity/logo.svg";
 import heartImage from "../../common/assets/image/charity/heart-red.png";
 
-const Navbar = ({ type }) => {
-  return (
-    <NavbarWrapper type={type} className="navbar">
-      <Container fullWidth={true}>
-        <Logo
-          href="/"
-          logoSrc={logoImage}
-          className="logo"
-          title="Charity React Next Landing"
-        />
-        <MenuWrapper>
-          <Link href="/partners">
-            <a>
-              <Button>
-                <span className="text">Γίνε Συνεργάτης</span>
-                <Image src={heartImage} alt="Charity Landing" />
-              </Button>
-            </a>
-          </Link>
-        </MenuWrapper>
-      </Container>
-    </NavbarWrapper>
-  );
-};
+const Navbar = ({ type }) => (
+  <NavbarWrapper type={type} className="navbar">
+    <Container fullWidth>
+      <Logo
+        href="/"
+        logoSrc={logoImage}
+        className="logo"
+        title="Charity React Next Landing"
+      />
+      <MenuWrapper>
+        <Link href="/partners">
+          <a>
+            <Button>
+              <span className="text">Γίνε Συνεργάτης</span>
+              <Image src={heartImage} alt="Charity Landing" />
+            </Button>
+          </a>
+        </Link>
+      </MenuWrapper>
+    </Container>
+  </NavbarWrapper>
+);
 
 export default Navbar;
